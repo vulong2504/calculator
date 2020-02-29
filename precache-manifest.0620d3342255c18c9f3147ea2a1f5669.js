57f6a83b5933f01996de2521ba5de1db4c1dc7e1
Mr. Rabbit <oldwhite37201@users.noreply.github.com>
2020-02-29 22:08:03 +0700

    Updates

---
+++ precache-manifest.0620d3342255c18c9f3147ea2a1f5669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c067ca476ec2ef9a595b6502cbfd9bd4",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "a04011db070f01f92868",
+    "url": "/calculator/static/css/main.51717810.chunk.css"
+  },
+  {
+    "revision": "5d320a77a9928325bd8c",
+    "url": "/calculator/static/js/2.2e0c87e2.chunk.js"
+  },
+  {
+    "revision": "a5288a4ed4e4beba8413aa5c9ae969ea",
+    "url": "/calculator/static/js/2.2e0c87e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04011db070f01f92868",
+    "url": "/calculator/static/js/main.977184fa.chunk.js"
+  },
+  {
+    "revision": "ccfc56ce1b0c91cb76b8",
+    "url": "/calculator/static/js/runtime-main.627f4c10.js"
+  }
+]);
